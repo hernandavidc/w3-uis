12d5312bbc29494d8de8908284534c0d42d5e348
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-07-22 21:33:05 -0500

    Clase 22 Julio VueJs

---
+++ intro-vuejs/components/computed-obj.js
@@ -0,0 +1,24 @@
+Vue.component('computed-obj', {
+    data(){
+        return{
+            valor: 0
+        }
+    },
+    computed:{
+        valorFormateado: {
+            get() {
+              return `$ ${ this.valor }`;
+            },
+            set(newVal) {
+                this.valor = newVal;
+            }
+        }
+    },
+    template: `
+        <div>
+            <h2> Componente de computed props como Obj </h2>
+            <input v-model="valor" />
+            <p> {{ valorFormateado }} </p>
+        </div>
+    `
+});
--- intro-vuejs/components/watcher.js
+++ intro-vuejs/components/watcher.js
@@ -6,18 +6,33 @@ Vue.component('watcher', {
         }
     },
     mounted(){
-        this.getDataUser();
+        // this.getDataUser();
     },
     methods:{
         async getDataUser(){
-            const data = await fetch('https://randomuser.me/api/');
-            const json = await data.json();
-            console.log(json);
+            try{
+                const data = await fetch('https://randomuser.me/api/');
+                const json = await data.json();
+                const user = json.results[0];
+                this.user = `${ user.name.title } ${ user.name.first } ${ user.name.last }`
+                console.log(user);
+            } catch(err) {
+                console.error(err);
+            }
+        }
+    },
+    watch: {
+        user(newVal, oldVal){
+            this.user = newVal;
+            this.oldUser = oldVal;
         }
     },
     template: `
         <div>
             <h2> Titulo watcher </h2>
+            <p @click="getDataUser"> Click para actualizar User </p>
+            <p> User actual: {{ user }} </p>
+            <p> Old user: {{ oldUser }} </p>
         </div>
     `
 })
