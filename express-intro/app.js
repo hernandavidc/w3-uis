798ffa32bf2e152e5248345041a26db185be76e0
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-08-04 21:35:29 -0500

    4 Agosto Express Js

--- express-intro/app.js
+++ express-intro/app.js
@@ -1,95 +1,35 @@
+const userRoutes = require('./routes/users');
 const express = require('express');
-const Joi = require('joi');
+const config = require('config');
+const initDebug = require('debug')('api:init');
+const dbDebug = require('debug')('api:db');
+const userValir = require('./middlewares/auth');
+const morgan = require('morgan');
 
 const app = express();
-app.use(express.json());
 
-const data = [
-    {cc: "10986545", name: "Hernan"}, 
-    {cc: "12345678", name: "Nat"}, 
-    {cc: "41235678", name: "Daniel"}
-]
+if(app.get('env') === 'dev'){
+    initDebug(' ---- Entorno de desarrollo ---- ')
+    console.log('Mogar middleware... ')
+    app.use(morgan('tiny'));
+}
+
+app.use(userValir);
+
+//Connect to DB
+console.log(`Db host: ${ config.get('dbConfig.host') }`);
+console.log(`Db port: ${ config.get('dbConfig.port') }`);
+dbDebug('--- Ok connect DB')
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use('/api/users', userRoutes)
 
 app.get('/', function(req, res){
     res.send('Estamos en el server de Express')
 });
 
-app.get('/api/users', function(req, res){
-    res.send(data)
-});
-
-//CREAR
-app.post('/api/users', (req, res) => {
-    const user = {
-        cc: req.body.cc,
-        name: req.body.name
-    }
-
-    const { value, error} = validateUser(user);
-    if(error){
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-
-    data.push(value);
-    res.send(value);
-})
-
-app.get('/api/users/:cc', function(req, res){
-    let user = exitsUser(req.params.cc)
-    if(!user) res.status(404).send("No hay registro con esa CC")
-    res.send(user)
-});
-
-//Update
-app.put('/api/users/:cc', (req, res) => {
-    let user = exitsUser(req.params.cc)
-    if(!user){
-        res.status(404).send("No hay registro con esa CC")
-        return;
-    }
-
-    const { value, error} = validateUser({
-        cc: req.body.cc,
-        name: req.body.name
-    });
-
-    if(error){
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-
-    user.cc = req.body.cc;
-    user.name = req.body.name;
-    res.send(user);
-})
-
-//Delete
-app.delete('/api/users/:cc', (req, res) => {
-    let user = exitsUser(req.params.cc)
-    if(!user){
-        res.status(404).send("No hay registro con esa CC")
-        return;
-    }
-
-    const indexUser = data.indexOf(user);
-    data.splice(indexUser, 1);
-    res.send(user);
-})
-
 app.listen(5000, () => {
     console.log("Servidor de express corriendo en el puerto 5000");
 })
-
-function exitsUser(cc){
-    return data.find(u => u.cc === cc)
-}
-
-function validateUser(user){
-    const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-        cc: Joi.string().alphanum().min(7).required()
-    })
-
-    return schema.validate(user);
-}
---
+++ express-intro/middlewares/auth.js
@@ -0,0 +1,6 @@
+function userValir(req, res, next) {
+    console.log('Auth running...')
+    next();
+}
+
+module.exports = userValir;
