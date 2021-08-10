c15a028898de5b2b58c8fb4c8d165b0b401979ba
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-08-09 20:05:48 -0500

    9 Agosto base Express

---
+++ base-proyecto/models/server.js
@@ -0,0 +1,37 @@
+const config = require('config');
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = config.get('PORT');
+        this.usuariosPath = '/api/usuarios';
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use( cors() );
+        this.app.use( express.json() );
+        this.app.use( express.static('public') );
+    }
+
+    routes() {
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
