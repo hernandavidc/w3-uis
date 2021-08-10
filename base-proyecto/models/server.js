037896e1a3a61509b699ec37913a5daa48d04510
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2021-08-09 21:35:14 -0500

    Sheet code

--- base-proyecto/models/server.js
+++ base-proyecto/models/server.js
@@ -2,6 +2,8 @@ const config = require('config');
 const express = require('express');
 const cors = require('cors');
 
+const { dbConnection } = require('../database/config')
+
 class Server {
 
     constructor() {
@@ -9,13 +11,19 @@ class Server {
         this.port = config.get('PORT');
         this.usuariosPath = '/api/usuarios';
 
+        this.initDB();
         this.middlewares();
         this.routes();
+    }
+
+    async initDB(){
+        await dbConnection();
     }
 
     middlewares() {
         this.app.use( cors() );
         this.app.use( express.json() );
+        this.app.use( express.urlencoded({extended: true}) );
         this.app.use( express.static('public') );
     }
 
@@ -30,8 +38,5 @@ class Server {
     }
 
 }
-
-
-
 
 module.exports = Server;
